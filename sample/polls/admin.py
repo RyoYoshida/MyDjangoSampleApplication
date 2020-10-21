b70119c2b8a1472ebd40e0811a9a88a3c9454da1
Ryo Yoshida <Ryo19810902@hotmail.com>
2020-10-22 01:20:42 +0900

    add admin functions

--- sample/polls/admin.py
+++ sample/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Answer
+from .models import Choice
+from .models import Question
+
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Answer)
