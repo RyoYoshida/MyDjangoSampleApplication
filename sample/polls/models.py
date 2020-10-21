9477b299a0090c9dcf4363c563c99ff29ec840f6
Ryo Yoshida <Ryo19810902@hotmail.com>
2020-10-22 00:17:24 +0900

    update polls migraion

--- sample/polls/models.py
+++ sample/polls/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=191)
     pub_date = models.DateTimeField('date published')
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=191)
     votes = models.IntegerField(default=0)
+
+
+class Answer(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
