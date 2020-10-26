33c63fb1efb60d1307538c3f33cb9f49232f0f86
Ryo Yoshida <Ryo19810902@hotmail.com>
2020-10-26 22:17:21 +0900

    update admin pages

--- sample/polls/admin.py
+++ sample/polls/admin.py
@@ -4,6 +4,35 @@ from .models import Answer
 from .models import Choice
 from .models import Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 2
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = []
+    inlines = [AnswerInline]
+
+
+admin.site.register(Choice, ChoiceAdmin)
+
+
 admin.site.register(Answer)
